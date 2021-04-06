61cb743e2a65e7dbffd7ed7c22f298d42de094b0
Adam Drago <atdrago@gmail.com>
2021-04-06 13:46:28 -0400

    fix(ci): Try adding cert and publishing in the same step

--- config/add-macos-cert.sh
+++ config/add-macos-cert.sh
@@ -7,17 +7,17 @@ MACOS_CERT_P12_FILE=certificate.p12
 echo $MACOS_CERTIFICATE | base64 --decode > $MACOS_CERT_P12_FILE
 
 #create a keychain
-security create-keychain -p actions $KEY_CHAIN
+security create-keychain -p tmp-password $KEY_CHAIN
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 
 # Unlock the keychain
-security unlock-keychain -p actions $KEY_CHAIN
+security unlock-keychain -p tmp-password $KEY_CHAIN
 
 security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k actions $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k tmp-password $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
