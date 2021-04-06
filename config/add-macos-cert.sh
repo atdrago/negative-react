fc697cdaff895ff1ca3ff2bd5c95a5265c2b8506
Adam Drago <atdrago@gmail.com>
2021-04-06 14:41:33 -0400

    fix(ci): Remove `codesign:` from set-key-partition-list

--- config/add-macos-cert.sh
+++ config/add-macos-cert.sh
@@ -17,7 +17,7 @@ security unlock-keychain -p tmp-password $KEY_CHAIN
 
 security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k tmp-password $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k tmp-password $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
